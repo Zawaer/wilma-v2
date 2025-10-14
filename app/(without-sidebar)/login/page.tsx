4c3b43e10cae56bb5cfbccf312d06035125795da
Zawaer <zawarudo123pro@gmail.com>
2025-10-14 13:31:32 +0200

    Added login page with custom password input

--- app/(without-sidebar)/login/page.tsx
+++ app/(without-sidebar)/login/page.tsx
@@ -1,7 +1,66 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { PasswordInput } from "@/components/password-input"
+import { Button } from "@/components/ui/button"
+
 export default function LoginPage() {
   return (
-    <div>
-      this is login
+    <div className="flex flex-col gap-4 w-screen h-screen items-center justify-center break-words">
+      <Card className="min-w-[25rem]">
+        <CardHeader className="flex flex-col items-center">
+          <CardTitle className="text-2xl font-bold">Log in to your account</CardTitle>
+          <CardDescription>Enter your Wilma credentials below to log in</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="username">
+                  Username
+                </FieldLabel>
+                <Input
+                id="username"
+                type="username"
+                placeholder="matti.heikkinen"
+                required />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="https://espoo.inschool.fi/forgotpasswd"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <PasswordInput id="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Log in</Button>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center break-words max-w-[25rem]">
+        By clicking continue, you agree to our <a href="/terms-of-service">Terms of Service</a>{" "}
+        and <a href="/privacy-policy">Privacy Policy</a>.
+      </FieldDescription>
     </div>
   )
 }
