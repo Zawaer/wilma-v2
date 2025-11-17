e0520c27276a2c5408984854f78c046664a6ad69
Zawaer <zawarudo123pro@gmail.com>
2025-11-17 13:53:02 +0200

    Add hand cursor to DropdownMenuItems

--- components/nav-user.tsx
+++ components/nav-user.tsx
@@ -76,7 +76,7 @@ export function NavUser({
                     >
                         <DropdownMenuLabel className="p-0 font-normal">
                             <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                                <Avatar className="h-8 w-8 rounded-lg">
+                                <Avatar className="h-8 w-8 rounded-lg select-none">
                                     <AvatarImage
                                         src={user.avatar}
                                         alt={user.name}
